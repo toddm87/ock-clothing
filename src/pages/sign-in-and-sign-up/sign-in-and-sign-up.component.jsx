6e0762095f728f28248f74bf872cffadf51196e8
Todd M <tboogie87@me.com>
2021-01-03 20:03:43 -0500

    added new features

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -5,7 +5,7 @@ import SignIn from '../../components/sign-in/sign-in.component'
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
-  <div className='sign-in-and-sign-up'>
+  <div className='sign-in-and-sign-up'> SIGN IN
     <SignIn />
   </div>
 )
